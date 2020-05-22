b89f76cc119b4491561db82b107cc1b58f03fbae
Henrique Tavares <ihenrits@gmail.com>
2020-05-22 10:11:54 -0300

    refactor: refactor 404 code

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-class NotFoundPage extends React.Component {
-  render() {
-    return (
-      <Layout location={this.props.location}>
-        <main>
-          <h1>Not Found</h1>
-          <p>Hey Coder, you're lost?</p>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/WpqUXd8XPmo"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullscreen
-          />
-          <p>I've been feeling lost lately...</p>
-        </main>
-      </Layout>
-    );
-  }
+export default function NotFoundPage({ location }) {
+  return (
+    <Layout location={location}>
+      <main>
+        <h1>Not Found</h1>
+        <p>Hey Coder, you are lost?</p>
+        <iframe
+          title="lost"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/WpqUXd8XPmo"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullscreen
+        />
+        <p>Ive been feeling lost lately...</p>
+      </main>
+    </Layout>
+  );
 }
-
-export default NotFoundPage;
